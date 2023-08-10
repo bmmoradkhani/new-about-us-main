01674325210d179829a6e0e2d878cba5d7042f89
Behnam <bm.moradkhani@gmail.com>
2023-08-10 16:07:40 +0330

    add start counter with scroll to new-about-us-v2

--- components/AboutUsPage/CounterNew.js
+++ components/AboutUsPage/CounterNew.js
@@ -1,25 +1,46 @@
-import { useRef } from "react";
-import { useCountUp } from "react-countup";
+import React from "react";
+import { useRef, useState } from "react";
+// import { useCountUp } from "react-countup";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const CounterNew = (props) => {
-  const {
-    data: { startNum, endNum, duration, delay, decimals },
-  } = props;
-  const countUpRef = useRef(null);
+  // const {
+  //   data: { startNum, endNum, duration, delay, decimals },
+  // } = props;
+  // const countUpRef = useRef(null);
 
-  const {} = useCountUp({
-    ref: countUpRef,
-    start: startNum,
-    end: endNum,
-    delay,
-    duration,
-    decimals,
-  });
+  // const {} = useCountUp({
+  //   ref: countUpRef,
+  //   start: startNum,
+  //   end: endNum,
+  //   delay,
+  //   duration,
+  //   decimals,
+  // });
+
+  const [counterOn, setCounterOn] = useState(false);
 
   return (
-    <>
-      <span ref={countUpRef}></span>
-    </>
+    <ScrollTrigger
+      style={{ display: "inline" }}
+      onEnter={() => setCounterOn(true)}
+      onExit={() => setCounterOn(false)}
+    >
+      {counterOn && (
+        <CountUp
+          // ref={countUpRef}
+          // start={'startNum'}
+          start={1}
+          end={5.1}
+          duration={2}
+          delay={0.4}
+          decimals={1}
+        />
+      )}
+
+      {/* <span ref={countUpRef}></span> */}
+    </ScrollTrigger>
   );
 };
 
--- components/AboutUsPage/index.tsx
+++ components/AboutUsPage/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "./AboutUsPage.module.scss";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
+
+import point from "../../public/assets/images/about-us/Group 670.png";
 
 import energyIcon from "../../public/assets/images/about-us/energy 1.png";
 import integration from "../../public/assets/images/about-us/integration 1.png";
@@ -9,10 +11,17 @@ import save_Money from "../../public/assets/images/about-us/save-money 1.png";
 import fuel from "../../public/assets/images/about-us/fuel 2.png";
 import handshake from "../../public/assets/images/about-us/handshake 1.png";
 
-import shopify from "../../public/assets/images/about-us/unnamed.png";
-// import shopify from "../../public/assets/images/about-us/shopify.png";
-import vector from "../../public/assets/images/about-us/Vector.png";
+// import wave from "../../public/assets/images/about-us/wave.png";
+// import wave1 from "../../public/assets/images/about-us/wave1.png";
+// import wave2 from "../../public/assets/images/about-us/wave2.png";
 
+// import persifindLine from "../../public/assets/images/about-us/Persifund line.png";
+
+// import shopify from "../../public/assets/images/about-us/unnamed.png";
+// // import shopify from "../../public/assets/images/about-us/shopify.png";
+// import vector from "../../public/assets/images/about-us/Vector.png";
+
+// import CountUp from "react-countup";
 import CounterNew from "./CounterNew";
 
 const data_hours = [
@@ -65,6 +74,10 @@ export default function index() {
         </div>
       </div>
 
+      <div className={`${styles} my-4`}>
+        <Image src={point} className="" alt="point" />
+      </div>
+
       <div className={`${styles.numberTitle}`}>
         <p>We're backed by incredible Numbers</p>
       </div>
@@ -73,9 +86,11 @@ export default function index() {
         className={`${styles.numberBox} d-flex justify-content-center align-items-center`}
       >
         <section className={`${styles.numberBox_Title}`}>
-          {data_hours.map((data, i) => (
+          {/* {data_hours.map((data, i) => (
             <CounterNew data={data} key={i} />
-          ))}{" "}
+          ))}{" "} */}
+          <CounterNew end={100} start={10} duration={1} />
+          {/* <CountUp /> */}
           <span className={`${styles.numberBox_Title_Hours}`}>hours</span>
           <span className={`${styles.numberBox_Title_span}`}>
             saved on return handling a month
@@ -137,10 +152,49 @@ export default function index() {
         <div
           className={`${styles.about_Persifund} col-12 col-md-6 my-3 my-md-0 p-0`}
         >
-          <h2>
-            About{" "}
-            <span className={`${styles.about_Persifund_span}`}>Persifund</span>
-          </h2>{" "}
+          <div className="">
+            <h2 className="d-flex justify-content-center">
+              About{" "}
+              <span className={`${styles.about_Persifund_span}`}>
+                Persifund
+              </span>
+            </h2>{" "}
+            <div
+              className={`${styles.about_Persifund_underline} d-flex justify-content-center`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="285"
+                height="14"
+                viewBox="0 0 285 14"
+                fill="none"
+              >
+                <path
+                  d="M2.80176 11.056C28.618 8.49995 93.3559 2.90848 145.784 2.90845C197.7 2.90841 232.169 2.90845 282.383 7.99891"
+                  stroke="url(#paint0_linear_578_1361)"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_578_1361"
+                    x1="0.247981"
+                    y1="13.531"
+                    x2="282.383"
+                    y2="11.056"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="#2A438F" />
+                    <stop offset="1" stop-color="#2A438F" stop-opacity="0.87" />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+          </div>
+
+          {/* <div className="col-11 text-end">
+            <Image alt="persifindLine" src={persifindLine} />
+          </div> */}
         </div>
 
         <div
@@ -169,146 +223,149 @@ export default function index() {
         <h1>Why we are here</h1>
       </div>
 
-      {/* //////////////////////////////      1       ///////////////////////////////// */}
+      {/* /////////////////////////////////////   /////////////////////////////////////// */}
 
-      <div className={`${styles.efficiency} row justify-content-center m-0`}>
-        <div
-          className={`${styles.efficiency_Title} p-0 col-12 col-sm-5 text-center text-lg-end`}
-        >
-          <h2>Efficiency</h2>
-        </div>
+      <div className={`${styles.center_Box}`}>
+        {/* //////////////////////////////      1       ///////////////////////////////// */}
 
-        <div className={`${styles.icon} p-0 col-2 d-none d-lg-inline`}>
-          <div className={`${styles.lineIcon_Energy}`}></div>
+        <div className={`${styles.efficiency} m-0`}>
+          <div
+            className={`${styles.efficiency_Title} p-0 col-12 col-sm-5 text-center text-sm-end`}
+          >
+            <h2>Efficiency</h2>
+          </div>
 
-          <div className={`${styles.Icon_Energy}`}>
-            <Image src={energyIcon} className="" alt="energyIcon" />
+          <div className={`${styles.icon} p-0  d-none d-sm-inline`}>
+            <div className={`${styles.lineIcon_Energy}`}></div>
+
+            <div className={`${styles.Icon_Energy}`}>
+              <Image src={energyIcon} className="" alt="energyIcon" />
+            </div>
+          </div>
+
+          <div className={`${styles.efficiency_context} p-0 col-12 col-sm-5 `}>
+            <p>
+              Our return management system is designed to optimize your
+              operations. By automating tasks and eliminating manual processes,
+              we help you handle returns quickly and efficiently, saving you
+              valuable time and resources.
+            </p>
           </div>
         </div>
 
-        <div className={`${styles.efficiency_context} p-0 col-12 col-sm-5 `}>
-          <p>
-            Our return management system is designed to optimize your
-            operations. By automating tasks and eliminating manual processes, we
-            help you handle returns quickly and efficiently, saving you valuable
-            time and resources.
-          </p>
-        </div>
-      </div>
+        {/* //////////////////////////////      2        ////////////////////////////////////// */}
 
-      {/* //////////////////////////////      2        ////////////////////////////////////// */}
+        <div className={`${styles.seamless} m-0 `}>
+          <div className={`${styles.seamless_context} p-0 col-12 col-sm-5`}>
+            <p>
+              Our seamless integration enables you to effortlessly integrate our
+              return management system into your existing workflow, ensuring a
+              smooth and cohesive experience for both you and your customers.
+            </p>
+          </div>
 
-      <div className={`${styles.seamless} row  justify-content-center m-0 `}>
-        <div
-          className={`${styles.seamless_context} p-0 col-12 col-sm-7 mx-auto mx-sm-0`}
-        >
-          <p>
-            Our seamless integration enables you to effortlessly integrate our
-            return management system into your existing workflow, ensuring a
-            smooth and cohesive experience for both you and your customers.
-          </p>
-        </div>
+          <div className={`${styles.icon_seamless} p-0 d-none d-sm-inline`}>
+            <div className={`${styles.lineIcon_Energy_seamless}`}></div>
 
-        <div className={`${styles.icon_seamless} p-0 col-1 d-none d-lg-inline`}>
-          <div className={`${styles.lineIcon_Energy_seamless}`}></div>
+            <div className={`${styles.Icon_Energy_seamless}`}>
+              <Image src={integration} className="" alt="Picture of the " />
+            </div>
+          </div>
 
-          <div className={`${styles.Icon_Energy_seamless}`}>
-            <Image src={integration} className="" alt="Picture of the " />
+          <div
+            className={`${styles.seamless_Title} p-0 col-12 col-sm-5 text-center text-sm-start`}
+          >
+            <h2>Seamless Integration</h2>
           </div>
         </div>
 
-        <div
-          className={`${styles.seamless_Title} p-0 col-12 col-sm-3 text-center text-lg-start`}
-        >
-          <h2>Seamless Integration</h2>
-        </div>
-      </div>
+        {/* //////////////////////////////      3        ///////////////////////////////////////////// */}
 
-      {/* //////////////////////////////      3        ///////////////////////////////////////////// */}
+        <div className={`${styles.costSavings} m-0`}>
+          <div
+            className={`${styles.costSavings_Title} p-0 col-12 col-sm-5 text-center text-lg-end`}
+          >
+            <h2>Cost Savings</h2>
+          </div>
 
-      <div className={`${styles.costSavings} row justify-content-center m-0`}>
-        <div
-          className={`${styles.costSavings_Title} p-0 col-12 col-sm-5 text-center text-lg-end`}
-        >
-          <h2>Cost Savings</h2>
-        </div>
+          <div className={`${styles.icon} p-0  d-none d-sm-inline`}>
+            <div className={`${styles.lineIcon_Energy}`}></div>
 
-        <div className={`${styles.icon} p-0 col-2 d-none d-lg-inline`}>
-          <div className={`${styles.lineIcon_Energy}`}></div>
+            <div className={`${styles.Icon_Energy}`}>
+              <Image src={save_Money} alt="save Money" />
+            </div>
+          </div>
 
-          <div className={`${styles.Icon_Energy}`}>
-            <Image src={save_Money} className="" alt="save Money" />
+          <div className={`${styles.costSavings_context} p-0 col-12 col-sm-5`}>
+            <p>
+              Our solution is built to help you save money. By streamlining your
+              return processes, reducing errors, and optimizing resource
+              allocation, we help you minimize costs associated with returns,
+              ultimately improving your bottom line.
+            </p>
           </div>
         </div>
 
-        <div className={`${styles.costSavings_context} p-0 col-12 col-sm-5`}>
-          <p>
-            Our solution is built to help you save money. By streamlining your
-            return processes, reducing errors, and optimizing resource
-            allocation, we help you minimize costs associated with returns,
-            ultimately improving your bottom line.
-          </p>
-        </div>
-      </div>
-
-      {/* /////////////////////////////       4       ////////////////////////////////////////////// */}
-
-      <div
-        className={`${styles.environmental} row justify-content-center m-0 flex-column-reverse flex-md-row`}
-      >
-        <div className={`${styles.environmental_context} p-0 col-12 col-sm-3`}>
-          <p>
-            We believe in sustainability. By digitizing and automating the
-            return process, we contribute to reducing paper waste and
-            unnecessary transportation, making a positive impact on the
-            environment
-          </p>
-        </div>
+        {/* /////////////////////////////       4       ////////////////////////////////////////////// */}
 
         <div
-          className={`${styles.icon_environmental} p-0 col-1 d-none d-lg-inline`}
+          className={`${styles.environmental} justify-content-center m-0 d-flex flex-column-reverse flex-sm-row`}
         >
-          <div className={`${styles.lineIcon_Energy_environmental}`}></div>
+          <div
+            className={`${styles.environmental_context} p-0 col-12 col-sm-5`}
+          >
+            <p>
+              We believe in sustainability. By digitizing and automating the
+              return process, we contribute to reducing paper waste and
+              unnecessary transportation, making a positive impact on the
+              environment
+            </p>
+          </div>
 
-          <div className={`${styles.Icon_Energy_environmental}`}>
-            <Image src={fuel} className="" alt="fuel" />
+          <div
+            className={`${styles.icon_environmental} p-0  d-none d-sm-inline`}
+          >
+            <div className={`${styles.lineIcon_Energy_environmental}`}></div>
+
+            <div className={`${styles.Icon_Energy_environmental}`}>
+              <Image src={fuel} className="" alt="fuel" />
+            </div>
+          </div>
+
+          <div
+            className={`${styles.environmental_Title} p-0 col-12 col-sm-5 text-center text-sm-start`}
+          >
+            <h2>Environmental Responsibility</h2>
           </div>
         </div>
 
-        <div
-          className={`${styles.environmental_Title} p-0 col-12 col-sm-4 text-center text-lg-end`}
-        >
-          <h2>Environmental Responsibility</h2>
-        </div>
-      </div>
+        {/* ////////////////////////////       5       /////////////////////////////////////////////// */}
 
-      {/* ////////////////////////////       5       /////////////////////////////////////////////// */}
+        <div className={`${styles.dedicated} m-0`}>
+          <div
+            className={`${styles.dedicated_Title} p-0 col-12 col-sm-5 text-center text-sm-end`}
+          >
+            <h2>Dedicated Support</h2>
+          </div>
 
-      <div className={`${styles.dedicated} row justify-content-center m-0`}>
-        <div
-          className={`${styles.dedicated_Title} p-0 col-12 col-sm-5 text-center text-lg-end`}
-        >
-          <h2>Dedicated Support</h2>
-        </div>
+          <div className={`${styles.icon} p-0 d-none d-sm-inline`}>
+            <div className={`${styles.lineIcon_Energy}`}></div>
 
-        <div className={`${styles.icon} p-0 col-2 d-none d-lg-inline`}>
-          <div className={`${styles.lineIcon_Energy}`}></div>
+            <div className={`${styles.Icon_Energy}`}>
+              <Image src={handshake} className="" alt="Picture of the " />
+            </div>
+          </div>
 
-          <div className={`${styles.Icon_Energy}`}>
-            <Image src={handshake} className="" alt="Picture of the " />
+          <div className={`${styles.dedicated_context} p-0 col-12 col-sm-5`}>
+            <p>
+              We are committed to your success. Our team of dedicated experts is
+              always ready to assist you, providing exceptional customer support
+              and guidance throughout your journey with our return management
+              system.
+            </p>
           </div>
         </div>
-
-        <div className={`${styles.dedicated_context} p-0 col-12 col-sm-5`}>
-          <p>
-            We are committed to your success. Our team of dedicated experts is
-            always ready to assist you, providing exceptional customer support
-            and guidance throughout your journey with our return management
-            system.
-          </p>
-        </div>
       </div>
-
       {/* /////////////////////////////////////////////////////////////////////////// */}
 
       <div
@@ -322,9 +379,9 @@ export default function index() {
 
       {/* ////////////////////////////////////////////// */}
 
-      <div className={`${styles.my_Container} mt-5 pt-5 row`}>
+      <div className={`${styles} mt-5 pt-5 row`}>
         <div
-          className={`${styles.delight}  col-12 col-lg-6 col-xl-7 mb-5 mb-lg-0 p-0`}
+          className={`${styles.delight} ${styles}  col-12 col-lg-6 col-xl-7 mb-5 mb-lg-0 p-0`}
         >
           <h2>Get Started and Delight Your Customers with ZERO cost NOW!</h2>
         </div>
